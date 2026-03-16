0569c97fa0d7adf812328bca5e611937b0d3cd25
ThibeauWouters <thibeauwouters@gmail.com>
2026-03-16 08:17:18 +0100

    Changing to float64

--- src/aspire_bilby/__init__.py
+++ src/aspire_bilby/__init__.py
@@ -1,6 +1,17 @@
 """aspire-bilby: A Bilby wrapper for aspire."""
 
 from importlib.metadata import PackageNotFoundError, version
+try:
+    import jax
+    jax.config.update("jax_enable_x64", True)
+except ImportError:
+    pass
+
+try:
+    import torch
+    torch.set_default_dtype(torch.float64)
+except ImportError:
+    pass
 
 try:
     __version__ = version(__name__)
